a1051854aa3076326b0ccdc23d0dd96b1f04549d
Joevdluxo <guic6010@gmail.com>
2024-07-20 22:00:25 -0300

    ex 16

--- cursojs/aula14ex/ex16/script.js
+++ cursojs/aula14ex/ex16/script.js
@@ -1,1 +1,31 @@
-window.alert('Olá')
+function contar(){
+
+    let ini = document.getElementById('txti') 
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = 'impossivel contar, faltam dados'
+    }else{
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0){
+            window.alert('[ERRO] Passo invalido, considerando PASSO 1')
+            p = 1
+        }
+    if(i<f){ // contagem crescente
+        for(let c = i; c <= f; c+=p){ 
+            res.innerHTML += `${c}\u{1F449}` 
+        }
+    }else { // contagem decrescente
+        for(let c = i; c >= f; c-=p){
+            res.innerHTML += `${c}\u{1F449}`
+        }
+    }
+        res.innerHTML += `Fim da contagem`
+    }
+
+}
